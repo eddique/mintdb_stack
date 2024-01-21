8108863c316100f23de4be32630d93af7e93059d
Eric R <eric.rodriguez@gusto.com>
2024-01-21 18:51:27 -0500

    Starting config framework

---
+++ src/cli/config.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+use std::sync::OnceLock;
+
+pub static CF: OnceLock<Config> = OnceLock::new();
+
+pub struct Config {
+    pub path: String,
+    pub user: Option<String>,
+    pub pass: Option<String>,
+    pub crt: Option<PathBuf>,
+    pub key: Option<PathBuf>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config { path: format!("mint.db"), user: Some(format!("mint.db")), pass: Some(format!("mint.db")), crt: None, key: None }
+    }
+}
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,11 @@
+mod config;
 mod repl;
 use crate::db;
 use crate::net;
+
+use self::config::Config;
 pub async fn init() -> anyhow::Result<()> {
+    let _ = config::CF.set(Config::default());
     db::init().await?;
     let srv = tokio::spawn(net::init());
     srv.await?;
